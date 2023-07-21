c1df3e8cc26ad2aaaaac9930bfb357421e31366d
raa <freedom.toh3@gmail.com>
2023-07-21 22:01:57 +0700

    0.3.0 1) добавлена команда создания пользователя 2) Добавлены модели статусов и запросов, загружены фикстуры для модели статусов

---
+++ databases/init/pg_requests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql <<- EOSQL
+    CREATE USER $GPT_USER WITH PASSWORD '$GPT_PASSWORD';
+    CREATE DATABASE requests;
+    GRANT ALL PRIVILEGES ON DATABASE requests TO $GPT_USER;
+EOSQL
