e8ad96ad72db93d188439bd9ea0bcf02b74a78f3
raa <freedom.toh3@gmail.com>
2023-07-27 22:21:48 +0700

    1.0.0 Полноценная версия для прода

--- gpt_service/gpt_service/settings.py
+++ gpt_service/gpt_service/settings.py
@@ -1,4 +1,10 @@
-from gpt_service.configs.dev import *
+import os
+
+if os.getenv('DJANGO_PROD', False) in ["true", "True", True]:
+    from gpt_service.configs.prod import *
+else:
+    from gpt_service.configs.dev import *
+
 from gpt_service.configs.base import *
 from gpt_service.configs.database import *
 from gpt_service.configs.loggs import *
